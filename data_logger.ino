7be9ce14649acdf57d92c3d7851c1adfef2699f5
GDHAF <63614369+GDHAF@users.noreply.github.com>
2025-03-10 20:57:18 -0300

    Correção do Código do Data Logger
    Atualização e correção do código que antes estava presente o do semaforo

--- data_logger.ino
+++ data_logger.ino
@@ -1,103 +1,72 @@
-int carRed = 12; // assign the car lights
-int carYellow = 11;
-int carGreen = 10;
-int pedRed = 9; // assign the pedestrian lights
-int pedGreen = 8;
-int button = 2; // button pin
-int crossTime = 1000; // time allowed to cross
-unsigned long changeTime; // time since button pressed
-int led_1 = 3;
-int led_2 = 4;
-int led_3 = 5;
-int led_4 = 6;
-int led_5 = 7;
-int buzzer = 13;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//Configuração do LCD 
+#define col 16
+#define lin 2
+#define ende 0x27
+
+LiquidCrystal_I2C lcd(ende, col, lin);
+
+//Sensores
+const int dht11 = A3;
+const int ldr = A2;
+const int rtc = A1;
+
+//LED RGB
+const int red = 10;
+const int green = 11;
+const int blue = 12;
+
+//Extra
+const int btn1 = 2;
+const int btn2 = 3;
+const int buzzer = A0;
+
+//Variáveis de uso
+int min_luz = 0;
+int max_luz = 500;
 
 void setup() {
-    pinMode(carRed, OUTPUT);
-    pinMode(carYellow, OUTPUT);
-    pinMode(carGreen, OUTPUT);
-    pinMode(pedRed, OUTPUT);
-    pinMode(pedGreen, OUTPUT);
-    pinMode(button, INPUT); // button on pin 2
 
-    pinMode(led_1, OUTPUT);
-    pinMode(led_2, OUTPUT);
-    pinMode(led_3, OUTPUT);
-    pinMode(led_4, OUTPUT);
-    pinMode(led_5, OUTPUT);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
 
-    pinMode(buzzer, OUTPUT);
-    
-    // turn on the green light
-    digitalWrite(carGreen, HIGH);
-    digitalWrite(pedRed, HIGH);
+  pinMode(dht11, INPUT);
+  pinMode(ldr, INPUT);
+  pinMode(rtc, INPUT);
+
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+
+  pinMode(btn1, INPUT);
+  pinMode(btn2, INPUT);
+
+  pinMode(buzzer, OUTPUT);
+
 }
+
+
+
+void ajustarLuminosidade(){
+  Serial.println("Luminosidade Mínima");
+  min_luz = analogRead(ldr);
+  digitalWrite(red, HIGH);
+  delay(100);
+  digitalWrite(red, LOW);
+
+  Serial.println("Luminosidade Máxima");
+  max_luz = analogRead(ldr);
+  digitalWrite(red, HIGH);
+  delay(100);
+  digitalWrite(red, LOW);
+
+}
+
 
 void loop() {
-    int state = digitalRead(button);
-    /* check if button is pressed and it is
-    over 5 seconds since last button press */
-    if (state == HIGH && (millis() - changeTime) > 5000) {
-        // Call the function to change the lights
-        changeLights();
-    }
-}
+  
 
-void changeLights() {
-    digitalWrite(carGreen, LOW); // green off
-    digitalWrite(carYellow, HIGH); // yellow on
-    delay(2000); // wait 2 seconds
-
-    digitalWrite(carYellow, LOW); // yellow off
-    digitalWrite(carRed, HIGH); // red on
-    delay(1000); // wait 1 second till its safe
-
-    digitalWrite(pedRed, LOW); // ped red off
-    digitalWrite(pedGreen, HIGH); // ped green on
-    
-    digitalWrite(led_1, HIGH);
-    digitalWrite(led_2, HIGH);
-    digitalWrite(led_3, HIGH);
-    digitalWrite(led_4, HIGH);
-    digitalWrite(led_5, HIGH);
-   // delay(500); // wait for preset time period
-
-    delay(crossTime);
-    digitalWrite(led_1, LOW);
-    
-    delay(crossTime);
-    digitalWrite(led_2, LOW);
-
-    delay(crossTime);
-    digitalWrite(led_3, LOW);
-
-    delay(crossTime);
-    digitalWrite(led_4, LOW);
-
-    // flash the ped green
-    for (int x = 0; x < 5; x++) {
-        digitalWrite(pedGreen, HIGH);
-        tone(buzzer, 262, 100);
-        delay(100);
-
-        digitalWrite(pedGreen, LOW);
-        tone(buzzer, 392, 100);
-        delay(100);
-    }
-
-    digitalWrite(led_5, LOW);
-    delay(250);
-    // turn ped red on
-    digitalWrite(pedRed, HIGH);
-
-    //digitalWrite(carYellow, HIGH); // yellow on
-    digitalWrite(carRed, LOW); // red off
-    delay(1000);
-    digitalWrite(carGreen, HIGH);
-    //digitalWrite(carYellow, LOW); // yellow off
-
-    // record the time since last change of lights
-    changeTime = millis();
-    // then return to the main program loop
 }
